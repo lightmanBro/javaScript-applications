b081f86f9d0bfb753eb42ed9894ade255c1e9612
lightmanBro <omotosodavidromeo@gmail.com>
2022-07-06 10:09:53 +0100

    work in progress

--- app.js
+++ app.js
@@ -15,14 +15,16 @@ var controller = (function(budgetCtrl, UIctrl){
 
 
            document.querySelector('.add__btn').addEventListener('click', function(){
+                    
                     //1. Get the field input data
-
 
                     //2. Add the item to the budget controller
 
                     //3. Add the item to the UI
 
                     //4. Calculate the budget
+
+                    //5. Display the budget to the UI
            })
 
 })(budgetrController, UIcontroller );
