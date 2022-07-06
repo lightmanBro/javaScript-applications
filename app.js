46808b74cb09ebf499fca36d9a7d8100f732c89d
lightmanBro <omotosodavidromeo@gmail.com>
2022-07-06 10:04:58 +0100

    setup the js module structure

--- app.js
+++ app.js
@@ -14,4 +14,15 @@ var controller = (function(budgetCtrl, UIctrl){
 // this is where to control what happens upon each events and delegate the tasks to the other controllers.
 
 
+           document.querySelector('.add__btn').addEventListener('click', function(){
+                    //1. Get the field input data
+
+
+                    //2. Add the item to the budget controller
+
+                    //3. Add the item to the UI
+
+                    //4. Calculate the budget
+           })
+
 })(budgetrController, UIcontroller );
