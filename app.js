1e2470cdaa725567176935ea855de216cb3a441c
lightmanBro <omotosodavidromeo@gmail.com>
2022-07-06 09:47:14 +0100

    initialized the project

---
+++ app.js
@@ -0,0 +1,17 @@
+// BUDGET CONTROLLER
+var budgetrController = (function(){
+
+} )();
+
+// UI CONTROLLER
+var UIcontroller = (function(){
+
+
+})();
+
+// GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UIctrl){
+// this is where to control what happens upon each events and delegate the tasks to the other controllers.
+
+
+})(budgetrController, UIcontroller );
